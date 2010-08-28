166277028632cf7621e9421a2bce7288dea655c1
nathan.northcutt <nathan.northcutt@d97de159-6cea-38fe-db00-451556c7535c>
2010-08-28 00:34:17 +0000

    added a scrolling textblock instead of the telnet console view.  playing around with some stuff until i get it working.  noticing a lot of formatting issues with the text being displayed. not sure if i should try casting to ascii or what the deal is...gotta figure that one out.

---
+++ Gigamud/trunk/Gigamud.UI/Console/MainConsole.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Gigamud.Communications.Sockets.Telnet;
+
+namespace Gigamud.UI.Console
+{
+    public partial class MainConsole : UserControl
+    {
+        TelnetSocket _socket;
+        Run _currentRun;
+
+        public MainConsole()
+        {
+            InitializeComponent();
+            _socket = new TelnetSocket("69.205.222.240", 23);
+            _socket.MessageRecieved += new TelnetSocket.IncomingMessageHandler(_socket_MessageRecieved);
+            _socket.Connect();
+        }
+
+        void _socket_MessageRecieved(string message)
+        {
+            if (this.Dispatcher.CheckAccess())
+            {
+                Run r = new Run();
+                r.Text = message;
+                tblkConsole.Inlines.Add(r);
+                Viewer.UpdateLayout();
+                Viewer.ScrollToVerticalOffset(double.MaxValue);
+            }
+            else
+                this.Dispatcher.BeginInvoke(() => { _socket_MessageRecieved(message); });
+        }
+
+        private void tbxMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (_socket.IsConnected)
+                    _socket.Write(tbxMain.Text);
+                tbxMain.Text = string.Empty;
+            }
+        }
+    }
+}

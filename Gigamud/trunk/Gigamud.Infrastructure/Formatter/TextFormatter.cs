6d597dcdd2db1d97dc826dade7e307d2f1123370
nathan.northcutt <nathan.northcutt@d97de159-6cea-38fe-db00-451556c7535c>
2010-08-30 19:52:32 +0000

    added basic text formatter.  need to implement rules for controlling text colorization. also need to make sure only appropriate parts of lines are colorized.

---
+++ Gigamud/trunk/Gigamud.Infrastructure/Formatter/TextFormatter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Gigamud.Infrastructure.Formatter
+{
+    public static class TextFormatter
+    {
+        public static Run Format(string s)
+        {
+            Run r = new Run();
+            r.Text = s;
+            r.Foreground = new SolidColorBrush(Colorize(s));
+            return r;
+        }
+
+        static Color Colorize(string s)
+        {
+            if (s.Contains("say"))
+                return Colors.Green;
+            if (s.Contains("damage"))
+                return Colors.Red;
+            if (s.Contains("experience"))
+                return Colors.Yellow;
+            return Colors.Gray;
+        }
+    }
+}
